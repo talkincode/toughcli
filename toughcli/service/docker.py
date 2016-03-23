cd7d1633205ed24461567db7fbaf7bf55d36fa04
toughman <wjt.net@outlook.com>
2016-03-24 04:54:34 +0800

    wlan setup

--- toughcli/service/docker.py
+++ toughcli/service/docker.py
@@ -3,10 +3,10 @@
 import os
 
 def auto_install():
-    os.system("sudo curl -sSL https://get.daocloud.io/docker | sh")
-    os.system("sudo curl -L https://get.daocloud.io/docker/compose/releases/download"
+    os.system("curl -sSL https://get.daocloud.io/docker | sh")
+    os.system("curl -L https://get.daocloud.io/docker/compose/releases/download"
               "/1.5.2/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose")
-    os.system("sudo service docker start")
+    os.system("service docker start")
 
 
 
